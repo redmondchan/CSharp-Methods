f54a49491f33a66ccba4ece5c4fade49022692f1
Redmond Chan <redmondchan92@gmail.com>
2019-12-31 18:38:01 +0000

    added example of method with return

--- main.cs
+++ main.cs
@@ -6,10 +6,17 @@ class MainClass {
     Console.WriteLine("Hello");
   }
 
+  static int Sum(int x){
+    int result = x + x;
+    return result;
+  }
+
   public static void Main (string[] args) {
     Console.WriteLine ("Method");
 
   SayHi();
+
+  Console.WriteLine(Sum(3));
 
   }
 }
