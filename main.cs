f4e8671c4e702e8e5cc7301699cafcbbffc20a89
Redmond Chan <redmondchan92@gmail.com>
2019-12-31 18:35:19 +0000

    added example for void method

--- main.cs
+++ main.cs
@@ -1,7 +1,15 @@
 using System;
 
 class MainClass {
+
+  static void SayHi(){
+    Console.WriteLine("Hello");
+  }
+
   public static void Main (string[] args) {
     Console.WriteLine ("Method");
+
+  SayHi();
+
   }
 }
